9c7c237bf58a57e9db3778449be9eb354135a456
EC2 Default User <ec2-user@ip-172-31-13-0.ap-northeast-1.compute.internal>
2021-10-09 13:10:25 +0000

    contoroller create

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
 
   get 'admin' => "admin/homes#top", as: "admin"
 
-  #namespace :admin do
-   #resources :products, only: [:new, :index, :create, :show, :edit, :update]
+  namespace :admin do
+   resources :products, only: [:new, :index, :create, :show, :edit, :update]
    #resources :genres, only: [:index, :create, :edit, :update]
    #resources :orders, only: [:show, :update]
    #resources :order_details, only: [:update]
    #resources :customers, only: [:index, :show, :edit, :update]
-  #end
+  end
 
  #scope module: :public do
   #get 'homes/about' => "homes#about"
