3f9206847fb3b8c4b34ae69643d59d2806a9f3ea
EC2 Default User <ec2-user@ip-172-31-13-0.ap-northeast-1.compute.internal>
2021-10-09 12:26:37 +0000

    controller create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,35 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  #root to: 'public/homes#top'
+
+  get 'admin' => "admin/homes#top", as: "admin"
+
+  #namespace :admin do
+   #resources :products, only: [:new, :index, :create, :show, :edit, :update]
+   #resources :genres, only: [:index, :create, :edit, :update]
+   #resources :orders, only: [:show, :update]
+   #resources :order_details, only: [:update]
+   #resources :customers, only: [:index, :show, :edit, :update]
+  #end
+
+ #scope module: :public do
+  #get 'homes/about' => "homes#about"
+  #post 'orders/verification' => "orders#verification"
+  #get 'orders/verification' => "orders#verification"
+  #get 'orders/completion' => "orders#completion"
+  #get 'customers/withdrawal' => "customers#withdrawal"
+  #put 'customers/proces' => "customers#proces"
+  #resources :items, only: [:index, :show]
+  #resources :orders, only: [:new, :index, :show, :create]
+  #resource :customers, only: [:show, :edit, :update]
+  #resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  #resources :cart_items, only: [:index, :update, :destroy, :create] do
+    #member do
+      #delete :delete
+    #end
+  #end
+
+
+
+ end
+
