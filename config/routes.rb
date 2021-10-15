dd8b49208f9d04b6c787287f67db7e2975dd64f3
sowa-takei <souwasora@gmail.com>
2021-10-15 09:30:07 +0000

    会員詳細画面完了

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
    resources :genres, only: [:index, :create, :edit, :update]
    #resources :orders, only: [:show, :update]
    #resources :order_details, only: [:update]
-   #resources :customers, only: [:index, :show, :edit, :update]
+   resources :customers, only: [:index, :show, :edit, :update]
   end
 
  scope module: :public do
