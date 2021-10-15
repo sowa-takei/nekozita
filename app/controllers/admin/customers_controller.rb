a5f804c25451b0fbf9ab038e7618493219312e0b
sowa-takei <souwasora@gmail.com>
2021-10-15 15:52:29 +0000

    会員情報編集終了

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,9 +8,17 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+     @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customers_path(@customer)
   end
 
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name,:last_name_kana,:first_name,:first_name_kana,:postal_code,:address,:telephone_number,:is_active,:gender,:email)
+  end
 end
