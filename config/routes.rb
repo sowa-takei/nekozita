980be2a1669e13185f1f7440366fbd9f657706a1
sowa-takei <souwasora@gmail.com>
2021-10-20 14:06:40 +0000

    注文履歴詳細途中まで

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,8 @@ Rails.application.routes.draw do
   namespace :admin do
    resources :items, only: [:new, :index, :create, :show, :edit, :update]
    resources :genres, only: [:index, :create, :edit, :update]
-   #resources :orders, only: [:show, :update]
-   #resources :order_details, only: [:update]
+   resources :orders, only: [:show, :update]
+   resources :order_details, only: [:update]
    resources :customers, only: [:index, :show, :edit, :update]
   end
 
