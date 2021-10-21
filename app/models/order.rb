fcfd83be5036c3c58a37afdd2dfc722c6ad96eb8
sowa-takei <souwasora@gmail.com>
2021-10-21 14:27:26 +0000

    注文履歴詳細

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,4 +8,5 @@ class Order < ApplicationRecord
 
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { waiting_for_payment: 0,　payment_confirmation: 1, production: 2,　ready_to_ship: 3,　sent: 4 }
+  
 end
