b62d2cf8cefe6f7d988ce384cd096351ea5b5194
sowa-takei <souwasora@gmail.com>
2021-10-29 03:38:23 +0000

    レビュー追加

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -8,5 +8,6 @@ class Public::ItemsController < ApplicationController
     @cart_item = CartItem.new
     @post_comment = PostComment.new
     @post_comments = @item.post_comments.all
+    @reviews = @item.reviews.all
   end
 end
