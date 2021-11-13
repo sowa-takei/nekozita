9e40a9e6208db8d62b91f168f9acc031fa19190d
sowa-takei <souwasora@gmail.com>
2021-11-13 14:12:19 +0000

    add　問い合わせ機能

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,13 @@ module Nekozita
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/*.yml').to_s]
 
+    config.before_configuration do
+    env_file = File.join(Rails.root, 'config', 'local_env.yml')
+    YAML.load(File.open(env_file)).each do |key, value|
+      ENV[key.to_s] = value
+    end if File.exists?(env_file)
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
