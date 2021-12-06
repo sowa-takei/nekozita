3800e0a9860f8c1fb2688e88b7c16b7fe8136fe9
sowa-takei <souwasora@gmail.com>
2021-12-06 13:59:48 +0000

    [add]バリデーション追加

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,6 +2,8 @@ class CartItem < ApplicationRecord
   belongs_to :item
   belongs_to :customer
 
+  validates :amount, inclusion: { in: (1..10) }
+
   def subtotal
     item.with_tax_price * amount
   end
