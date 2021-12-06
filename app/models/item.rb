b8dd9cf4debf77f10886c4199f39a17529a83d6c
sowa-takei <souwasora@gmail.com>
2021-12-06 14:03:40 +0000

    インデント整理

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,6 +9,8 @@ class Item < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :reviews, dependent: :destroy
 
+  
+
   def favorited_by?(customer)
     favorites.where(customer_id: customer.id).exists?
   end
